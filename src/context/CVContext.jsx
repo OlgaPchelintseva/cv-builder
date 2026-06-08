0ef03602511cf0678f1288e4348430cf0d42f8c1
StartSoft <college@gmail.com>
2026-06-08 20:26:27 +0700

    Create CV context and reduser

--- src/context/CVContext.jsx
+++ src/context/CVContext.jsx
@@ -1,9 +1,69 @@
-import { useState, useContext, createContext } from "react";
+import React, { useState, useContext, createContext, useReducer } from "react";
 
-export const CVContext = createContext();
+const CVContext = createContext();
 
 const initialState = () => {
-    
-}
+    try{
+        const resume = localStorage.getItem('cv_data');
+        if (resume) return JSON.parse(resume);
+    }
+    catch(error){
+        console.log(`Возникла ошибка ${error}`)
+    }
+    return {
+        personalInfo: {fullName: '', email: '', phone: '', position: ''},
+        experience: [{id: 1, company: '', role: '', years: ''}],
+        theme: 'classic'
+    };
+};
+
+function cvReduser(state, action){
+    switch (action.type) {
+        case 'UPDATE_PERSONAL':
+            return {
+                ...state,
+                personalInfo: {
+                    ...state.personalInfo,
+                    ...action.payload
+                }
+            };
+        case 'ADD_EXPERIENCE':
+            const newID = Date.now().toString();
+            return{
+                ...state,
+                experience: [
+                    ...state.experience, 
+                    {
+                        id: newID,
+                        company: '',
+                        role: '',
+                        years: ''
+                    }
+                ]
+            };
+        case 'UPDATE_EXPERIENCE':
+            const {id, updatedFields} = action.payload;
+            return {
+                ...state,
+                experience: state.experience.map(item => item.id === id ? {...item, ...updatedFields} : item)
+            };
+        case 'SET_THEME':
+            return {
+                ...state,
+                theme: action.payload
+            };
+        default:
+            return state;
+    }
+};
+
+export function CVProvider({children}){
+    const [state, dispatch] = useReducer(cvReduser, initialState());
 
 
+    return(
+        <CVContext.Provider value={{state, dispath}}>
+            {children}
+        </CVContext.Provider>
+    );
+};
