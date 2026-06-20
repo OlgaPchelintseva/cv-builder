1ca3a0b1c8b32b24c6cd60ca9ba008df00e86a1c
Olga Pchelintseva <gxjkrf998@yandex.ru>
2026-06-20 22:26:21 +0700

    Add progress bar

--- src/components/FormPanel.jsx
+++ src/components/FormPanel.jsx
@@ -38,9 +38,45 @@ function FormPanel(){
         dispatch({type: 'ADD_EDUCATION'})
     };
 
+    const calculateProgress = () => {
+        let progressCount = 0;
+
+        if (state.personalInfo.fullName && state.personalInfo.email && state.personalInfo.phone && state.personalInfo.position){
+            progressCount += 25;
+        }
+        if (state.personalInfo.email || state.personalInfo.phone){
+            progressCount += 25;
+        }
+
+        const isExperienceFilled = (exp) => {
+        return exp.company.trim() !== '' && exp.role.trim() !== '' && exp.years.trim() !== '';
+        };
+        const filledExperience = state.experience.filter(isExperienceFilled);
+        if (filledExperience.length > 0){
+            progressCount += 25;
+        }
+
+        const isEducationFilled = (edu) => {
+            return edu.institution.trim() !== '' && edu.degree.trim() !== '' && edu.years.trim() !== '';
+        };
+        const filledEducation = state.education.filter(isEducationFilled);
+        if (filledEducation.length > 0){
+            progressCount += 25;
+        }
+
+        return Math.min(progressCount, 100);
+    }
+
+    const progress = calculateProgress();
+
 
     return(
         <div className={styles.formPanelContainer}>
+            <h4>Индикатор заполненности резюме</h4>
+            <div className={styles.progressBarBox}>
+                <div style={{width: `${progress}%`, background: '#E9B1A3'}} className={styles.progressBar}></div>
+                <p>Заполнено: {progress}%</p>
+            </div>
             <div>
                 <h3>Личная информация</h3>
                 <div className={styles.info}>
